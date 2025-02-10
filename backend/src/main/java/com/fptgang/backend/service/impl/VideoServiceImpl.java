86a9ea32ca9e2ff15c59a14a4cd15819026e3fd9
hoangminh11 <lehoangminh20041108@gmail.com>
2025-02-10 16:33:09 +0700

    SWD-44: upload video unboxing

--- backend/src/main/java/com/fptgang/backend/service/impl/VideoServiceImpl.java
+++ backend/src/main/java/com/fptgang/backend/service/impl/VideoServiceImpl.java
@@ -2,26 +2,39 @@ package com.fptgang.backend.service.impl;
 
 import com.fptgang.backend.model.Video;
 import com.fptgang.backend.repository.VideoRepos;
+import com.fptgang.backend.service.AzureBlobService;
 import com.fptgang.backend.service.VideoService;
 import com.fptgang.backend.util.OpenApiHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
+@Slf4j
 public class VideoServiceImpl implements VideoService {
 
     private final VideoRepos videoRepos;
-
+    private final AzureBlobService azureBlobService;
     @Autowired
-    public VideoServiceImpl(VideoRepos videoRepos) {
+    public VideoServiceImpl(VideoRepos videoRepos, AzureBlobService azureBlobService) {
         this.videoRepos = videoRepos;
+        this.azureBlobService = azureBlobService;
     }
 
     @Override
-    public Video create(Video video) {
-        return videoRepos.save(video);
+    public Video create(Video video, MultipartFile file) {
+        try {
+            video.setUrl(azureBlobService.upload(file, file.getName()));
+            return videoRepos.save(video);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -30,11 +43,17 @@ public class VideoServiceImpl implements VideoService {
     }
 
     @Override
-    public Video update(Video video) {
+    public Video update(Video video, MultipartFile file) {
         if (video.getVideoId() == null) {
             throw new IllegalArgumentException("Video does not exist");
         }
-        return videoRepos.save(video);
+        try {
+            video.setUrl(azureBlobService.upload(file, file.getName()));
+            return videoRepos.save(video);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
