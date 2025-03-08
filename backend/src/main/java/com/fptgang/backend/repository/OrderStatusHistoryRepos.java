bbd85efd4c1f024f99382f5ffc5c4580ab86e15b
hoangminh11 <lehoangminh20041108@gmail.com>
2025-03-08 07:50:33 +0700

    Update OrderStatusHistoryRepos.java

--- backend/src/main/java/com/fptgang/backend/repository/OrderStatusHistoryRepos.java
+++ backend/src/main/java/com/fptgang/backend/repository/OrderStatusHistoryRepos.java
@@ -6,20 +6,19 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface OrderStatusHistoryRepos extends JpaRepository<OrderStatusHistory, Long>, JpaSpecificationExecutor<OrderStatusHistory> {
-    @Query(value = """
-    SELECT DATE(o.created_at) AS createdDate, 
-           AVG(TIMESTAMPDIFF(HOUR, o.created_at, osh.created_at)) AS avgFulfillmentTime
-    FROM order_status_history osh
-    JOIN `order` o ON osh.order_id = o.order_id
+    @Query("""
+    SELECT FUNCTION('DATE_FORMAT', o.createdAt, '%Y-%m-%d') AS createdDate,
+           AVG(FUNCTION('TIMESTAMPDIFF', 'HOUR', o.createdAt, osh.createdAt)) AS avgFulfillmentTime
+    FROM OrderStatusHistory osh
+    JOIN osh.order o
     WHERE osh.state = 'DELIVERED'
-    GROUP BY createdDate
-    ORDER BY createdDate
-""", nativeQuery = true)
+    GROUP BY FUNCTION('DATE_FORMAT', o.createdAt, '%Y-%m-%d')
+    ORDER BY MIN(o.createdAt)
+""")
     List<Object[]> getDailyFulfillmentTime();
 }
 
