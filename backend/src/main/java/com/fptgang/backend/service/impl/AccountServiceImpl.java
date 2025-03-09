41934edf8d47358a0df8a65a9ecc55b9c12f5136
hoangminh11 <lehoangminh20041108@gmail.com>
2025-03-09 18:49:53 +0700

    Update AccountServiceImpl.java

--- backend/src/main/java/com/fptgang/backend/service/impl/AccountServiceImpl.java
+++ backend/src/main/java/com/fptgang/backend/service/impl/AccountServiceImpl.java
@@ -41,6 +41,7 @@ public class AccountServiceImpl implements AccountService {
     public Account update(Account account) {
         Account existing = accountRepos.findById(account.getAccountId())
                 .orElseThrow(() -> new InvalidInputException("Account does not exist"));
+        existing.setPassword(account.getPassword());
         EntityUtil.merge(existing, account);
         return accountRepos.save(existing);
     }
